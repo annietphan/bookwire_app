2327f2c7aae76fcb25b6f0546feed0a9e538a4e2
Annie Phan <anniephan.atp@gmail.com>
2019-06-13 16:52:48 -0700

    add book & club

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   resources :clubs
   devise_for :users, :controllers => {registrations: 'registrations', :omniauth_callbacks => 'callbacks' }
   root to: 'application#welcome'
