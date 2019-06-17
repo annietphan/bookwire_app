083098c728db98961f6f8855d88dafaed266b7c4
Annie Phan <anniephan.atp@gmail.com>
2019-06-17 16:57:02 -0700

    add show function to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
-
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -23,4 +26,9 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :author, :summary)
   end
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
 end
