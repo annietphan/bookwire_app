400b2e45ee9d10b3be3dd5b9d1426cd9dcb8a6cc
Annie Phan <anniephan.atp@gmail.com>
2019-06-16 22:23:11 -0700

    add new & create to bookcontroller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,36 @@
 class BooksController < ApplicationController
+  before_action :find_book
 
-  # def index
-  #   @books = Book.all
-  # end
-  #
-  # def show
-  # end
-  #
-  # def new
-  #   @book = Book.new
-  #   @book.clubs.new
-  # end
-  #
-  # def create
-  #   @book = current_user.books.new
-  # end
-  #
-  # private
-  #
-  # def find_book
-  #   @book = Book.find(params[:id])
-  # end
-  #
-  # def book_params
-  #   params.require
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = current_user.books.build
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+
+    if @book.save
+      @book.reviews.create(user_id: current_user.id)
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :author, :summary)
+  end
+
 end
