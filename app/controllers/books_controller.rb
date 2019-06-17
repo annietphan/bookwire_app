08252850725fe5bd6bc4bff8d4c2d31ec82d9a55
Annie Phan <anniephan.atp@gmail.com>
2019-06-17 16:24:17 -0700

    delete views to start from scratch

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,55 +1,6 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all
-  end
-
-  def show
-
-  end
-
-  def new
-    @book = current_user.books.build
-    @user = current_user
-  end
-
-  def create
-    @book = current_user.books.build(book_params)
-
-    if @book.save
-      redirect_to user_book_path(current_user, @book), notice: "Succesfully created book!"
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    find_book
-    if @book.update(book_params)
-      redirect_to user_book_path(current_user, @book), notice: "Succesfully updated book!"
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @book.destroy
-    redirect_to books_path, notice: "Succesfully deleted book!"
-
-  end
-
-  private
-
-  def set_book
-    @book = Book.find(params[:id])
-  end
-
-  def book_params
-    params.require(:book).permit(:title, :genre, :author, :summary)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  resources :books do
-    resources :reviews
-  end
-  resources :users do
-    resources :books
-  end
-  # resources :clubs do
-  #   resources :books, only: [:index, :new, :create]
-  # end
+  resources :books
+  root 'books#index'
 
   devise_for :users, :controllers => {registrations: 'registrations', :omniauth_callbacks => 'callbacks' }
   root to: 'application#welcome'
