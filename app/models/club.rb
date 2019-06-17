8e29a659477eb300bd2d06febffda6f1d330bb59
Annie Phan <anniephan.atp@gmail.com>
2019-06-16 21:24:37 -0700

    comment out clubs model

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,10 +1,10 @@
-class Club < ApplicationRecord
-  has_many :books, inverse_of: :club
-  has_many :users, through: :books
-  validates :name, presence: true
-  validates :genre, presence: true
-  validates :description, presence: true
-
-  accepts_nested_attributes_for :books, reject_if: proc { |attributes| attributes[:name].blank?}, allow_destroy: true
-
-end
+# class Club < ApplicationRecord
+#   has_many :books, inverse_of: :club
+#   has_many :users, through: :books
+#   validates :name, presence: true
+#   validates :genre, presence: true
+#   validates :description, presence: true
+#
+#   accepts_nested_attributes_for :books, reject_if: proc { |attributes| attributes[:name].blank?}, allow_destroy: true
+#
+# end
