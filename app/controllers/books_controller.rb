851cd1b713d29cca2aa715c9779bc804ebfa0a44
Annie Phan <anniephan.atp@gmail.com>
2019-06-16 20:21:51 -0700

    add routes to book controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+    @book.clubs.new
+  end
+
+  def create
+    @book = current_user.books.new
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+  
 end
--- app/models/club.rb
+++ app/models/club.rb
@@ -1,6 +1,9 @@
 class Club < ApplicationRecord
   has_many :books, inverse_of: :club
   has_many :users, through: :books
+  validates :name, presence: true
+  validates :genre, presence: true
+  validates :description, presence: true
 
   accepts_nested_attributes_for :books, reject_if: proc { |attributes| attributes[:name].blank?}
 
