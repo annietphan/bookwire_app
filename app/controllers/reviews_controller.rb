033336c758a104e64d21a223daf9fa4792e6180c
Annie Phan <anniephan.atp@gmail.com>
2019-06-17 00:34:36 -0700

    trying to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,33 @@
 class ReviewsController < ApplicationController
+  before_action :set_book_and_user, only: [:new, :create]
+  before_action :find_review, only: [:edit, :update]
+
+  def new
+    @user = current_user
+
+  end
+
+  def create
+    if @review.save
+      redirect_to book_path(@review.book)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_book_and_user
+    @review = Review.new(user_id: current_user, book_id: params[:book_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :recommend)
+  end
+
+
 end
