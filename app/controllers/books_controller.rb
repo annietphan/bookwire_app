61840308a5b414285a54f80f4cbd305deb883855
Annie Phan <anniephan.atp@gmail.com>
2019-06-17 17:01:55 -0700

    add UD function to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,23 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @book.destroy
+    redirect_to root_path
+  end
+
   private
 
   def book_params
