358d79da87066c567d6d9d884e6f018e25406266
Annie Phan <anniephan.atp@gmail.com>
2019-06-17 19:38:40 -0700

    add delete function for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_book
+  before_action :set_review, only: [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -19,6 +20,24 @@ class ReviewsController < ApplicationController
 
   end
 
+  def edit
+  end
+
+  def update
+
+    if @review.update(review_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review.destroy
+
+    redirect_to book_path(@book)
+  end
+
   private
 
   def review_params
@@ -27,6 +46,10 @@ class ReviewsController < ApplicationController
 
   def set_book
     @book = Book.find(params[:book_id])
+  end
+
+  def set_review
+    @review = @book.reviews.find(params[:id])
   end
 
 end
